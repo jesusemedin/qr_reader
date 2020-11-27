e4b0a0b41f34f038b58c4f225b237c4877cbbc06
Jesus Medina <medinabotamail@gmail.com>
2020-11-27 19:16:17 -0400

    provider bottom navigation added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+
+import 'package:qr_reader/providers/ui_provider.dart';
 
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
-import 'package:qr_reader/pages/mapas_page.dart';
-import 'package:qr_reader/pages/direcciones_page.dart';
+
+
 
 class HomePage extends StatelessWidget {
   @override
@@ -30,8 +36,11 @@ class HomePage extends StatelessWidget {
 class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // OBTENER EL SELECTED MENU OPT
+    final uiProvider = Provider.of<UiProvider>(context);
+
     // EL CURRENT INDEX ES LO QUE SE NECESITA CAMBIAR PARA MOSTRAR LA PAGINA RESPECTIVA
-    final currentIndex = 0;
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     switch (currentIndex) {
       case 0:
--- lib/widgets/custom_navigatorbar.dart
+++ lib/widgets/custom_navigatorbar.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/providers/ui_provider.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final currentIndex = 0;
+    
+    final uiProvider = Provider.of<UiProvider>(context);
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     return BottomNavigationBar(
+      onTap: ( int i ) => uiProvider.selectedMenuOpt = i,
       currentIndex: currentIndex,
       elevation: 0,
       items: <BottomNavigationBarItem>[
